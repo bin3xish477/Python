b59ca568630abfcaa05ad07eacfdcfef7b4e925c
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2020-02-23 18:31:57 -0500

    Delete Account_ext.py

--- AccountCreator/Account_ext.py
+++
@@ -1,6 +0,0 @@
-from account import Account
-
-class Account():
-	def __repr__(self):
-		return f"Account : {self._name}, {self._email}, {self._verif_ques},\
-		{self._birth_date}, {self._password}, {self._phone_num}, {self._age}"
