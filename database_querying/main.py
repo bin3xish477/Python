e019c8d3229b41725bd2424f958e4225ff56a1ed
Alexis Rodriguez <44281620+rodriguez10011999@users.noreply.github.com>
2020-02-24 11:22:29 -0500

    Update main.py

--- database_querying/main.py
+++ database_querying/main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 try:
+  # --> import os for performing os functions
   import os
+  # --> import sqlite3 storing our csv file data into a database
   import sqlite3
-  import argparse
+  # --> import csv for parsing our csv file
+  import csv
+  # --> import colored for creeating a nicer looking interface
   import colored
+# --> handling an importing module error
 except ImportError:
-  print('%s[-] Error importing module %s' % (fg(1),attr(0)))
+  print('%s[-] Error importing module%s' % (fg(1),attr(0)))
