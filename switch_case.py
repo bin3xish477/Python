807367e56c5cf0f88889c34a01ac003d520d2f51
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-03-15 21:49:46 -0400

    updating switch_case.py

--- switch_case.py
+++ switch_case.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
-def dow_switch(day_num: int):
-  
+
+import random
+
+
+def dow_switch(day_num:int):
+  dict_dow = {
+      1: lambda: print('Monday'),
+      2: lambda: print('Tuesday'),
+      3: lambda: print('Wednesday'),
+      4: lambda: print('Thursday'),
+      5: lambda: print('Friday'),
+      6: lambda: print('Saturday'),
+      7: lambda: print('Sunday')
+  }
+
 
 def main():
-  
+  day_num = random.randint(1,7)
+  print('Day of the week is: ', day_num)
+
+
+if __name__ == '__main__':
+    main()
