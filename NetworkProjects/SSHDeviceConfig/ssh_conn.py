94b52555698cdb72fac19ebe75e6e82a3549ee22
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-07-16 22:28:59 -0400

    updating SSHDeviceConfig folder

--- NetworkProjects/SSHDeviceConfig/ssh_conn.py
+++ NetworkProjects/SSHDeviceConfig/ssh_conn.py
@@ -3,3 +3,12 @@ from sys import exit
 from os import path
 from time import sleep
 from re import search
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		sys.exit(1)
