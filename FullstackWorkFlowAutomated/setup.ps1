f029a84b2d4f55743d5c63d37dc95015eaaa107a
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2020-11-22 14:24:19 -0500

    Delete setup.ps1

--- FullstackWorkFlowAutomated/setup.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-WebRequest -Uri "https://chromedriver.storage.googleapis.com/87.0.4280.20/chromedriver_win32.zip" -Outfile "~/Documents/chromedriver.zip"
-Expand-Archive -LiteralPath "~/Documents/chromedriver.zip" -DestinationPath "~/Documents"
