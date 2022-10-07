aa2c5a9110cf8923a4a75676c740542b7369399a
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-10-07 12:13:33 -0400

    Add files via upload

--- Testing/pytest/tests/test_shopping_cart.py
+++ Testing/pytest/tests/test_shopping_cart.py
@@ -3,25 +3,36 @@
 from src.shopping_cart import ShoppingCart
 import pytest
 
-cart: ShoppingCart = ShoppingCart(1000)
+@pytest.fixture
+def cart():
+    return ShoppingCart(100)
 
-def test_can_item_to_cart():
+def test_can_item_to_cart(cart) -> None:
     cart.add("apple")
     assert cart.size == 1
 
-def test_when_item_added_cart_contains_item():
+def test_when_item_added_cart_contains_item(cart) -> None:
     cart.add("pineapple")
-    assert "pineapple" in cart.get_items()
+    assert "pineapple" in cart.items
 
-def test_get_items_from_cart():
-    cart.clear()
+def test_get_items_from_cart(cart) -> None:
     cart.add("orange")
     cart.add("juice")
-    assert cart.get_items() == [ "orange", "juice" ]
+    assert cart.items == [ "orange", "juice" ]
 
-def test_max_items_fail():
-    cart.clear()
+def test_max_items_fail(cart) -> None:
     with pytest.raises(OverflowError):
-        for i in range(1001):
+        for i in range(101):
             cart.add(i)
+
+def test_can_get_total_price(cart) -> None:
+    cart.add("shoes")
+    cart.add("milk")
+
+    price_map = {
+        "shoes": 100.0,
+        "milk": 4.50
+    }
+
+    assert cart.get_total_price(price_map) == 104.50
 
